036b4ca46279055b7d5d9451dd97c6d5d059fa36
storrer <storrer.chris@gmail.com>
2022-03-08 18:05:46 -0800

    Updated the project to its final form.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Tip Calculator in Python
 This script will ask for the cost of the food, the number of people splitting the bill, and the tip percentage. It will then calculate the total bill and the amount each person should pay based on the inputs. Then, it will print the total bill and the amount each person should pay.
+
+## How to use this script
+To use this script from the command line, run the following command:
+```shell
+python tip_calculator.py
+```
--- tip_calculator.py
+++ tip_calculator.py
@@ -5,19 +5,31 @@
     - Percentage of the tip
 
     It then calculates the total bill and the tip amount.
+    Inputs:
+    * Food costs $5000
+    * 876 people paying
+    * 12% tip
+
+    Expected output:
+    * `Total bill: $6,100.00`
+    * `Each person should pay $6.96`
+
 """
 # Function to calculate the total bill for each person and the tip amount
 def calculate_tip():
     # Get the cost of the food from the user and convert to float
-    total_food_cost = float(input("How much was the food? ")) 
+    food_cost = float(input("How much was the food? ")) 
     # Get the number of people splitting the bill from the user and convert to int
     num_people = int(input("How many people are splitting the bill? ")) 
     # Get the percentage of the tip from the user and convert to float
     tip_percentage = float(input("What percentage would you like to tip? "))
-    # Use f-strings to print the tip amount, the total bill, and the total cost per person
-    print(f'The tip amount is ${total_food_cost * (tip_percentage / 100)}')
-    print(f'The total bill is ${total_food_cost * (1 + tip_percentage / 100)}')
-    print(f'The total amount per person is ${total_food_cost / num_people}')
-
+    # Calculate the tip amount
+    tip_amount = food_cost * (tip_percentage / 100)
+    total_food_cost = food_cost + tip_amount
+    cost_per_person = total_food_cost / num_people
+    # Print the total bill in the format: `Total bill: $6,100.00`
+    print(f"Total bill: ${total_food_cost:.2f}")
+    # Print the tip amount in the format: `Each person should pay $6.96`
+    print(f"Each person should pay ${cost_per_person:.2f}")
 # Start the program
 calculate_tip()
